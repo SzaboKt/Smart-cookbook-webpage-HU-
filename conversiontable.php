61d4da7f8ecb65c5e85e3b57b915089b15ede11e
szkat <sz.katalin.00528@gmail.com>
2022-01-01 15:10:23 +0100

    test

---
+++ conversiontable.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>Konverziós táblázatok</title>
+    <meta charset="UTF-8"/>
+    <meta name="author" content="Szabó Katalin">
+    <meta name="keywords" content="szakácskönyv, mit főzzek ma?, meglévő hozzávalókból étel, okos szakácskönyv, konverziós táblázat">
+    <link rel="icon" href="ramenicon.png">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul class="user">
+            <li class="user_list"><a class="user_commands" href="mainmenu.php" target="_self">Vissza a főoldalra</a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>Okos szakácskönyv </h1>
+        <p>Itt vannak a konverziós táblázatok a különböző mértékegységeknek</p>
+    </header>
+    <section class="table_placing">
+        <table>
+            <caption>Csésze (Cup) - Gram - Uncia (Ounce)</caption>
+            <thead>
+                <tr>
+                    <th id="cup">Csésze</th>
+                    <th id="gram">Gram</th>
+                    <th id="ounce">Uncia</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td headers="cup">1/8 cup</td>
+                    <td headers="gram">16 g</td>
+                    <td headers="ounce">0.563 oz</td>
+                </tr>
+                <tr>
+                    <td headers="cup">1/4 cup</td>
+                    <td headers="gram">32 g</td>
+                    <td headers="ounce">1.13 oz</td>
+                </tr>
+                <tr>
+                    <td headers="cup">1/3 cup</td>
+                    <td headers="gram">43 g</td>
+                    <td headers="ounce">2.25 oz</td>
+                </tr>
+                <tr>
+                    <td headers="cup">1/2 cup</td>
+                    <td headers="gram">64 g</td>
+                    <td headers="ounce">2.25 oz</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+    <section class="table_placing">
+        <table>
+            <caption>Csésze (Cup) - Mililiter</caption>
+            <thead>
+            <tr>
+                <th id="cup">Csésze</th>
+                <th id="ml">Mililiter</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td id="cup">1 cup</td>
+                <td id="ml">250 ml</td>
+            </tr>
+            <tr>
+                <td id="cup">3/4 cup</td>
+                <td id="ml">175 ml</td>
+            </tr>
+            <tr>
+                <td id="cup">1/2 cup</td>
+                <td id="ml">125 g</td>
+            </tr>
+            </tbody>
+        </table>
+    </section>
+</body>
+<footer>
+    <aside class="aside_list">
+        <p>Néhány hasznos link</p>
+        <ul class="user">
+            <li><a class="aside_list_active" href="conversiontable.php">Konverziós táblázatok</a></li>
+        </ul>
+    </aside>
+</footer>
+</html>
---
+++ mainmenu.php
@@ -0,0 +1,88 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>Okos szakácskönyv</title>
+    <meta charset="UTF-8"/>
+    <meta name="author" content="Szabó Katalin">
+    <meta name="keywords" content="szakácskönyv, mit főzzek ma?, meglévő hozzávalókból étel, okos szakácskönyv">
+    <link rel="icon" href="ramenicon.png">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body id="generatorPage">
+    <nav>
+        <ul class="user">
+            <li class="user_list"><a class="user_commands" href="registration.php" target="_self">Regisztráció</a></li>
+            <li class="user_list"><a class="user_commands" href="login.php" target="_self">Bejelentkezés</a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>Okos szakácskönyv </h1>
+        <p>Ez az oldal arra szolgál, hogy már meglévő hozzávalókból recepteket generáljon, hogy mit is lehet belőlük főzni.</p>
+    </header>
+    <main id="asd">
+        <div id="main_page_content">
+            <p><span id="gen_style">Hogyan kell használni a generátort?</span></p>
+            <ol>
+                <li>Válaszd ki a legördülő listából/írd be a hozzávalók nevét.</li>
+                <li>Nyomj a "Generálás" gombra.</li>
+                <li>Kész is vagy! Válogathatsz a megjelenő receptek közül.</li>
+            </ol>
+        </div>
+        <div id="form_mainmenu">
+            <label id="ingredient_label" for="ingredients">Írj be egy hozzávalót!</label>
+            <input  list="ingredient_list" id="ingredients" name="ingredients"/>
+            <datalist id="ingredient_list">
+                <script>
+                    let l=[];
+                    l[0]="alma";
+                    l[1]="banán";
+                    l[2]="csoki";
+
+                    let options="";
+                    for (let i = 0; i < l.length; i++) {
+                        options += '<option value="' + l[i] + '" />';
+                    }
+
+                    document.getElementById("ingredient_list").innerHTML=options;
+                </script>
+            </datalist>
+            <button type="button" id="add_ingredient" name="add_ingedient">Hozzáadás</button>
+            <button type="button" id="generate" name="generate">Generálás</button><br/>
+        </div>
+    </main>
+    <script>
+        let ingredients="";
+        function iadd(){
+            if (document.contains(document.getElementById("paragraph"))) {
+                document.getElementById("paragraph").remove();
+            }
+            const p = document.createElement("p");
+            p.id="paragraph";
+            let i=document.getElementById("ingredients")
+            ingredients+=i.value + " ";
+            let node = document.createTextNode(ingredients);
+            p.appendChild(node);
+            const parent = document.getElementById("form_mainmenu");
+            parent.appendChild(p);
+
+            let l=document.getElementById("ingredient_list");
+            l.removeChild(i);
+        }
+        const e=document.getElementById("add_ingredient");
+        e.addEventListener ("click", iadd)
+    </script>
+</body>
+
+<footer>
+    <aside class="aside_list">
+        <p>Néhány hasznos link</p>
+        <ul class="user">
+            <li><a class="aside_list_format" href="conversiontable.php">Konverziós táblázatok</a></li>
+        </ul>
+    </aside>
+</footer>
+</html>
